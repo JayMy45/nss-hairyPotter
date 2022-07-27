e0fb0936f7c1721bd0ae7b5376cce907d5aac2e6
Jeremy Myers <echelonical@gmail.com>
2022-07-27 16:07:30 -0500

    potteryList module added...working code

---
+++ src/scripts/PotteryList.js
@@ -0,0 +1,40 @@
+import { usePottery } from "./PotteryCatalog.js"
+
+/* 
+1. Define and export a PotteryList function.  (I think it needs a parameter)
+
+2. The PotteryList function must get the items to be sold from the PotteryCatalog.js module (use invoked function from PotteryCatalog as parameter).
+- import PotteryCatalog.js
+- define variable invoking usePottery import
+
+3. The PotteryList function must convert each object in the array to an HTML representation string. Use the following template to generate the representations.
+
+<section class="pottery" id="pottery--1">
+  <h2 class="pottery__shape">Mug</h2>
+  <div class="pottery__properties">
+    Item weighs 3 grams and is 6 cm in height
+  </div>
+  <div class="pottery__price">Price is $20</div>
+</section>
+
+4. declare variable to store htmlString
+*/
+export const PotteryList = () => {
+    // Invoke the function that you imported from the database module
+    let storedPottery = usePottery();
+
+    //start building a string of HTML tags starting with below article tag...
+    let potteryHTMLString = `` //this tag anchors to exact same tag on index.html file.
+
+
+    for (let listObject of storedPottery) {
+        potteryHTMLString += `<section class="pottery" id="pottery--${listObject.id - 2}">
+        <h2 class="pottery__shape">${listObject.shape}</h2>
+        <div class="pottery__properties">
+          Item weighs ${listObject.weight} grams and is ${listObject.height} cm in height</div>
+        <div class="pottery__price">Price is $${listObject.price}</div>
+      </section>`
+    }
+    // potteryHTMLString += `</article>`
+    return potteryHTMLString
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,32 @@
 import { makePottery } from './PotteryWheel.js'
 import { firePottery } from './Kiln.js'
 import { toSellOrNotToSell } from './PotteryCatalog.js'
+import { PotteryList } from './PotteryList.js'
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery('flowerPot', 1, 17)
+let mug = makePottery('Mug', 1, 17)
+let ornament = makePottery("Ornament", 2, 5)
+let platter = makePottery("Platter", 7, 2)
 
 
 
 
 // Fire each piece of pottery in the kiln
-let firePot = firePottery(mug, 5)
+let fireMug = firePottery(mug, 5)
+let fireOrnament = firePottery(ornament, 5)
+let firePlatter = firePottery(platter, 5)
 
 // Determine which ones should be sold, and their price
 let sellOrNot = toSellOrNotToSell(mug, 5)
+let sellOrNot = toSellOrNotToSell(ornament, 5)
+let sellOrNot = toSellOrNotToSell(platter, 5)
 
 // Invoke the component function that renders the HTML list
 
+const listeD = PotteryList()
 
+const parentHTMLElement = document.querySelector(".potteryList")     // (".potteryList") is the class the function looks for in index.html to place scripted HTMLString from PotteryList
 
-console.log(firePot)
+parentHTMLElement.innerHTML = listeD
+
+// console.log(firePot)
